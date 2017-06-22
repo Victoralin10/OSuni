918ee59e3fafc94defb8bf205f26d262676568b2
Victor Cueva <Ingvcueva@gmail.com>
2017-06-21 20:34:18 -0500

    Updating tests.

--- src/main/java/pe/edu/uni/fiis/so/simulation/Cpu.java
+++ src/main/java/pe/edu/uni/fiis/so/simulation/Cpu.java
@@ -1,12 +1,8 @@
 package pe.edu.uni.fiis.so.simulation;
 
-
 /**
  * Created by vcueva on 6/21/17.
  */
-public class Kernel {
-
-    public void run() {
-
-    }
+public class Cpu {
+    
 }
--- src/test/java/StatisticTest.java
+++ src/test/java/StatisticTest.java
@@ -10,42 +10,48 @@ public class StatisticTest {
     @Test
     public void testUniform() {
         StatisticModel sm = StatisticParser.parse("uniform(-20, 100)");
-        Assert.assertNotNull(sm);
+        Assert.assertNotNull("Parsing uniform(-20, 100)", sm);
+
+        System.out.println("Testing: uniform(-20, 100)");
 
         for (int i = 0; i < 200; i++) {
             long v = sm.nextLongRandom();
 
-            Assert.assertTrue("Value generated out of range.", v >= -20 && v <= 100);
+            Assert.assertTrue("Value generated out of range:" + v, v >= -20 && v <= 100);
         }
     }
 
     @Test
     public void testExponential() {
         StatisticModel sm = StatisticParser.parse("exp(-1.5)");
-        Assert.assertNotNull(sm);
+        Assert.assertNotNull("Parsing exp(-1.5)", sm);
+
+        System.out.println("Testing: exp(-1.5)");
 
         for (int i = 0; i < 200; i++) {
             double v = sm.nexDoubleRandom();
 
-            Assert.assertTrue("Value generated out of range.", v >= 0);
+            Assert.assertTrue("Value generated out of range: " + v, v >= 0);
         }
     }
 
     @Test
     public void testNormal() {
         StatisticModel sm = StatisticParser.parse("normal(50, 10)");
-        Assert.assertNotNull(sm);
+        Assert.assertNotNull("Parsing normal(50, 10)", sm);
     }
 
     @Test
     public void testList() {
         StatisticModel sm = StatisticParser.parse("int[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
-        Assert.assertNotNull(sm);
+        Assert.assertNotNull("Parsing int[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", sm);
+
+        System.out.println("Testing: int[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
 
         for (int i = 0; i < 20; i++) {
             long v = sm.nextLongRandom();
 
-            Assert.assertTrue("Value not contains list", v > 0 && v <= 10);
+            Assert.assertTrue("Value not contains list: " + v, v > 0 && v <= 10);
         }
     }
 }
